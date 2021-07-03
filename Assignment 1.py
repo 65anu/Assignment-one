5ccf1096725c57a6c247556af2d7295baf559f3f
65anu <86882672+65anu@users.noreply.github.com>
2021-07-03 19:57:00 +0530

    Create Assignment 1.py

---
+++ Assignment 1.py
@@ -0,0 +1,13 @@
+
+ a=int(input("enter first number:"))
+b=int(input("enter second number"))
+sum=a+b
+Sub=a-b
+Mul=a*b
+div1=a//b
+div2=a/b
+print("addition: ",sum)
+print("Substraction: ",Sub)
+print("Multiplication: ",Mul)
+print("Integer division: ",div1)
+print("Floating point division: ",div2)
